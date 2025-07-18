e57317e5be245af69c12677ddfde4114f83a92a7
MFlores43DC <matthew.flores@dcmail.ca>
2025-07-18 13:39:40 -0400

    A4P2: main.js partially completed

--- Assignment 4/Part 2/gallery-start/main.js
+++ Assignment 4/Part 2/gallery-start/main.js
@@ -5,14 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images=["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
+const altTexts=["A closeup of an eye on someone's face","A picture showing some kind of stone-like pattern","A picture of purple flowers","A picture of Egyptian wall art","A picture of a butterfly on a leaf"]
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let ii=0;i<images.length;i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${images[i]}`);
+    newImage.setAttribute('alt', altTexts[i]);
+    thumbBar.appendChild(newImage);
+}
+
 
 /* Wiring up the Darken/Lighten button */
